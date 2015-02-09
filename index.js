610fd13226631820d5d642c94327141b6ad23537
Alex Coplan <lexy0202@gmail.com>
2015-02-09 17:46:14 +0000

    dealystart

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ var oneHz = Math.PI*2;
 // global vars
 var clockCache = 0;
 var frequency = 440;
-var pulseCounter = new Counter(8);
+var pulseCounter = new Counter(32);
+var BPM = 120;
+var beatFreq = BPM/60;
+var delayUnit = new Delay(4,0.8,0);
 
 function Counter(upto) { // counts from 0 -> upto - 1
   this.n = upto;
@@ -21,23 +24,40 @@ Counter.prototype.count = function() {
   return this.i;
 }
 
+function Delay(division, mix, feedback) {
+  this.numSamples = Math.round(sampleRate / (division*beatFreq*4));
+  this.mix = mix;
+  this.feedback = feedback;
+  this.buffer = [];
+  for (var i = 0; i < this.numSamples; i++) {
+    this.buffer.push(0.0);
+  }
+}
+
+Delay.prototype.process = function(sample) {
+  var delay = this,
+      delayed = delay.buffer.pop();
+  delay.buffer.unshift(sample + delay.feedback*delayed);
+  return sample + delay.mix*delayed;
+}
+
 /* to be called on every clock edge
  * rising => implies rising clock edge */
 function tick(rising) {
   //frequency = (frequency === 440) ? 550 : 440;
-  pulseCounter.count();
+  if (rising) pulseCounter.count(); // ticks every 1/32 beat
 }
 
 export function dsp(t) {
-  var clock = Math.sin(4*oneHz*t) > 0; // 1Hz clock
-  if (clock !== clockCache) tick(clock === 1); // clock = 1 => rising clock edge
+  var clock = Math.sin(16*beatFreq*oneHz*t) > 0; // ticks every 1/64 beat
+  if (clock !== clockCache) tick(clock); // clock = 1 => rising clock edge
   clockCache = clock;
   
   var amplitude = 0;
   
-  if (pulseCounter.i === 0) {
-    amplitude = 1;
+  if (pulseCounter.i < 6) {
+    amplitude = 1.0;
   }
   
-  return amplitude * Math.sin(frequency*oneHz*t);
+  return delayUnit.process(amplitude * Math.sin(frequency*oneHz*t));
 }
