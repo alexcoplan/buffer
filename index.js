b7b54f6e609eb9d4d05377a9ca47a9d1c495c8cf
Alex Coplan <lexy0202@gmail.com>
2015-02-09 14:38:09 +0000

    clocky

--- index.js
+++ index.js
@@ -3,6 +3,23 @@
  * @name buffer
  */
 
+// constants
+var oneHz = Math.PI*2;
+
+// global vars
+var clockCache = 0;
+var frequency = 440;
+
+/* to be called on every clock edge
+ * rising => implies rising clock edge */
+function tick(rising) {
+  frequency = (frequency === 440) ? 550 : 440;
+}
+
 export function dsp(t) {
-  return 0;
+  var clock = Math.sin(oneHz*t) > 0; // 1Hz clock
+  if (clock !== clockCache) tick(clock === 1); // clock = 1 => rising clock edge
+  clockCache = clock;
+  
+  return Math.sin(frequency*oneHz*t);
 }
