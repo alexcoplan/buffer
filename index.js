c763e5f4207facdf5f1e651ded9076b125dc9129
Alex Coplan <lexy0202@gmail.com>
2015-02-12 17:04:33 +0000

    beautdelay

--- index.js
+++ index.js
@@ -7,14 +7,25 @@
 var oneHz = Math.PI*2;
 
 // global vars
-var notes = [440,550,660,880,770,660,550,440,330];
+var notes = [440,550,660,880,770,660,550,495,330];
 var clockCache = 0;
 var frequency = 440;
-var pulseCounter = new Counter(8);
+var pulseCounter = new Counter(16);
 var noteCounter = new Counter(notes.length);
 var BPM = 120;
 var beatFreq = BPM/60;
 var delayUnit = new Delay(11128,.8,.6);
+var amplitude = 0;
+
+function increaseTo(source, interval, target) {
+  if (source + interval >= target) return target;
+  return source + interval;
+}
+
+function decreaseTo(source, interval, target) {
+  if (source - interval <= target) return target;
+  return source - interval;
+}
 
 function Counter(upto) { // counts from 0 -> upto - 1
   this.n = upto;
@@ -58,11 +69,12 @@ export function dsp(t) {
   var clock = Math.sin(16*beatFreq*oneHz*t) > 0; // ticks every 1/64 beat
   if (clock !== clockCache) tick(clock); // clock = 1 => rising clock edge
   clockCache = clock;
-  
-  var amplitude = 0;
 
   if (pulseCounter.i < 6) {
-    amplitude = 0.3;
+    amplitude = increaseTo(amplitude,0.005,0.4); // increase to 0.3 in intervals of 0.01
+  }
+  else {
+    amplitude = decreaseTo(amplitude,0.005,0); // decrease to 0 in intervals of 0.01
   }
   
   return delayUnit.process(amplitude * Math.sin(notes[noteCounter.i]*oneHz*t));
